914db57e7e176403ecaaec481d132632a4ee530c
Harry Parkes <harrydparkes@protonmail.com>
2021-03-15 13:04:35 +1300

    LCD printing GPS data

--- thecode1.ino
+++ thecode1.ino
@@ -1,12 +1,28 @@
 #include <Adafruit_GPS.h>
+#include <LiquidCrystal_I2C.h>
 
 #define GPSSerial Serial
 #define GPSECHO false
 
 Adafruit_GPS GPS(&GPSSerial);//Define GPS at serial port
+LiquidCrystal_I2C lcd(0x27,16,2);//Define LCD reselution as 16 x 2
 
 void setup()
 {
+  lcd.begin();//initialise LCD
+	lcd.backlight();//Enable backlight
+
+  lcd.setCursor(0,0);
+  lcd.print("Lat: ");
+  lcd.setCursor(0,1);
+  lcd.print("Lon: ");
+  lcd.setCursor(0,2);
+  lcd.print("Fix: ");
+  lcd.setCursor(12,2);
+  lcd.print("Qlt: ");
+  lcd.setCursor(0,3);
+  lcd.print("Sat: ");
+
   Serial.begin(115200);//Starts serial comunication with computer
   GPS.begin(9600);//Starts comuntication between GPS and Arduino
 
@@ -40,24 +56,30 @@ void loop()
     timer = millis(); // reset the timer
     if (GPS.fix)
     {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4);
-      Serial.print(GPS.lat);
-      Serial.print(", ");
-      Serial.print(GPS.longitude, 4);
-      Serial.println(GPS.lon);
-
-      Serial.print("Speed (knots): ");
-      Serial.println(GPS.speed);
-      Serial.print("Angle: ");
-      Serial.println(GPS.angle);
-      Serial.print("Altitude: ");
-      Serial.println(GPS.altitude);
-
-      Serial.print("Satellites: ");
-      Serial.println((int)GPS.satellites);
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
     }
+    lcd.setCursor(5, 0);
+    lcd.print(GPS.latitude, 5);
+    
+    lcd.setCursor(18, 0);
+    lcd.print(GPS.lat);
+    
+    lcd.setCursor(5, 1);
+    lcd.print(GPS.longitude, 5);
 
+    lcd.setCursor(18, 1);
+    lcd.print(GPS.lon);
+
+    lcd.setCursor(5, 2);
+    lcd.print((int)GPS.fix);
+
+    lcd.setCursor(18, 2);
+    lcd.print((int)GPS.fixquality);
+
+    lcd.setCursor(5, 3);
+    lcd.print((int)GPS.satellites);
   }
   delay(50);
 }
