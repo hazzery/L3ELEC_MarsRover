6b67c846138eac61f923051105f3676a77df6195
Harry Parkes <harrydparkes@protonmail.com>
2021-04-30 11:01:33 +1200

    Copy+pasted gyro functions

--- include/gyro.h
+++ include/gyro.h
@@ -7,6 +7,10 @@ class Gyro
         Gyro();
         Gyro(uint8_t Address);
 
+        void init();
+        int FunctionsPitchRoll(double A, double B, double C);
+        void Functions();
+
     private:
         uint8_t address;
 };
--- src/gyro.cpp
+++ src/gyro.cpp
@@ -4,3 +4,41 @@
 
 Gyro::Gyro(uint8_t Address)
  : address(Address) {}
+
+void Gyro::init()
+{
+  Wire.begin();
+
+  Wire.beginTransmission(address);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
+  delay(1000);
+}
+ 
+
+int Gyro::FunctionsPitchRoll(double A, double B, double C)
+{
+  double DatoA, DatoB, Value;
+  DatoA = A;
+  DatoB = (B*B) + (C*C);
+  DatoB = sqrt(DatoB);
+  
+  Value = atan2(DatoA, DatoB);
+  Value = Value * RAD_TO_DEG;
+  
+  return Value;
+}
+
+void Gyro::Functions()
+{
+  Wire.beginTransmission(address);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(address, 6, true);  // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read();      
+  AcY = Wire.read() << 8 | Wire.read();  
+  AcZ = Wire.read() << 8 | Wire.read();
+}
